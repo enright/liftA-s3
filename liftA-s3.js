22096c4964a8a1e5b1d8a882ebd02680c1a43d30
Bill Enright <bill@yisup.com>
2017-12-08 23:03:06 -0800

    all of s3

--- liftA-s3.js
+++ liftA-s3.js
@@ -32,7 +32,7 @@ SOFTWARE.
 
   module.exports = function (s3Config) {
   	return ((s3) => {
-  		let cb = (x, cont, p, advance, err, data) => {
+  		function cb(x, cont, p, advance, err, data) {
   			if (err) {
   				x = arw.Error(err, x);
   			} else {
@@ -40,27 +40,131 @@ SOFTWARE.
   			}
   			advance();
   			cont(x, p);
-  		};
+  		}
 
-  		let getObjectA = (reqParams) => function (x, cont, p) {
-  			let cancelId;
-  			let advance = () => p.advance(cancelId);
-  			let req = s3.getObject(reqParams(x), cb.bind(undefined, x, cont, p, advance));
-  			cancelId = p.add(() => req.abort());
-  			return cancelId;
-  		};
+      function s3ErrorBack(method) {
+        return (reqParams) => function (x, cont, p) {
+    			let cancelId;
+    			let advance = () => p.advance(cancelId);
+    			let req = s3[method](reqParams(x), cb.bind(undefined, x, cont, p, advance));
+    			cancelId = p.add(() => req.abort());
+    			return cancelId;
+    		};
+      }
 
-  		let putObjectA = (reqParams) => function (x, cont, p) {
-  			let cancelId;
-  			let advance = () => p.advance(cancelId);
-  			let req = s3.putObject(reqParams(x), cb.bind(undefined, x, cont, p, advance));
-  			cancelId = p.add(() => req.abort());
-  			return cancelId;
-  		};
+      function waitForA(state, params) {
+        return function (x, cont, p) {
+    			let cancelId;
+    			let advance = () => p.advance(cancelId);
+    			let req = s3.waitFor(state(x), params(x), cb.bind(undefined, x, cont, p, advance));
+    			cancelId = p.add(() => req.abort());
+    			return cancelId;
+        };
+      }
+
+      function getSignedURLA(operation, params) {
+        return function (x, cont, p) {
+    			let cancelId;
+    			let advance = () => p.advance(cancelId);
+    			let req = s3.waitFor(operation(x), params(x), cb.bind(undefined, x, cont, p, advance));
+    			cancelId = p.add(() => req.abort());
+    			return cancelId;
+        };
+      }
+
+      function uploadA(params, options) {
+        return function (x, cont, p) {
+    			let cancelId;
+          let req;
+    			let advance = () => p.advance(cancelId);
+          if (options) {
+            req = s3.upload(params(x), options(x), cb.bind(undefined, x, cont, p, advance));
+          } else {
+            req = s3.upload(params(x), cb.bind(undefined, x, cont, p, advance));
+          }
+    			cancelId = p.add(() => req.abort());
+    			return cancelId;
+        };
+      }
 
   		return {
-  			getObjectA: getObjectA,
-  			putObjectA: putObjectA
+        abortMultipartUploadA: s3ErrorBack('abortMultipartUpload'),
+        completeMultipartUploadA: s3ErrorBack('completeMultipartUpload'),
+        copyObjectA: s3ErrorBack('copyObject'),
+        createBucketA: s3ErrorBack('createBucket'),
+        createMultipartUploadA: s3ErrorBack('createMultipartUpload'),
+        createPresignedPostA: s3ErrorBack('createPresignedPost'),
+        deleteBucketA: s3ErrorBack('deleteBucket'),
+        deleteBucketAnalyticsConfigurationA: s3ErrorBack('deleteBucketAnalyticsConfiguration'),
+        deleteBucketCorsA: s3ErrorBack('deleteBucketCors'),
+        deleteBucketEncryptionA: s3ErrorBack('deleteBucketEncryption'),
+        deleteBucketInventoryConfigurationA: s3ErrorBack('deleteBucketInventoryConfiguration'),
+        deleteBucketLifecycleA: s3ErrorBack('deleteBucketLifecycle'),
+        deleteBucketMetricsConfigurationA: s3ErrorBack('deleteBucketMetricsConfiguration'),
+        deleteBucketPolicyA: s3ErrorBack('deleteBucketPolicy'),
+        deleteBucketReplicationA: s3ErrorBack('deleteBucketReplication'),
+        deleteBucketTaggingA: s3ErrorBack('deleteBucketTagging'),
+        deleteBucketWebsiteA: s3ErrorBack('deleteBucketWebsite'),
+        deleteObjectA: s3ErrorBack('deleteObject'),
+        deleteObjectsA: s3ErrorBack('deleteObjects'),
+        deleteObjectTaggingA: s3ErrorBack('deleteObjectTagging'),
+        getBucketAccelerateConfigurationA: s3ErrorBack('getBucketAccelerateConfiguration'),
+        getBucketAclA: s3ErrorBack('getBucketAcl'),
+        getBucketAnalyticsConfigurationA: s3ErrorBack('getBucketAnalyticsConfiguration'),
+        getBucketCorsA: s3ErrorBack('getBucketCors'),
+        getBucketEncryptionA: s3ErrorBack('getBucketEncryption'),
+        getBucketInventoryConfigurationA: s3ErrorBack('getBucketInventoryConfiguration'),
+        getBucketLifecycleConfigurationA: s3ErrorBack('getBucketLifecycleConfiguration'),
+        getBucketLocationA: s3ErrorBack('getBucketLocation'),
+        getBucketLoggingA: s3ErrorBack('getBucketLogging'),
+        getBucketMetricsConfigurationA: s3ErrorBack('getBucketMetricsConfiguration'),
+        getBucketNotificationConfigurationA: s3ErrorBack('getBucketNotificationConfiguration'),
+        getBucketPolicyA: s3ErrorBack('getBucketPolicy'),
+        getBucketReplicationA: s3ErrorBack('getBucketReplication'),
+        getBucketRequestPaymentA: s3ErrorBack('getBucketRequestPayment'),
+        getBucketTaggingA: s3ErrorBack('getBucketTagging'),
+        getBucketVersioningA: s3ErrorBack('getBucketVersioning'),
+        getBucketWebsiteA: s3ErrorBack('getBucketWebsite'),
+        getObjectA: s3ErrorBack('getObject'),
+        getObjectAclA: s3ErrorBack('getObjectAcl'),
+        getObjectTaggingA: s3ErrorBack('getObjectTagging'),
+        getObjectTorrentA: s3ErrorBack('getObjectTorrent'),
+        getSignedURLA: getSignedURLA,
+        headBucketA: s3ErrorBack('headBucket'),
+        headObjectA: s3ErrorBack('headObject'),
+        listBucketAnalyticsConfigurationsA: s3ErrorBack('listBucketAnalyticsConfigurations'),
+        listBucketInventoryConfigurationsA: s3ErrorBack('listBucketInventoryConfigurations'),
+        listBucketMetricsConfigurationsA: s3ErrorBack('listBucketMetricsConfigurations'),
+        listBucketsA: s3ErrorBack('listBuckets'),
+        listMultipartUploadsA: s3ErrorBack('listMultipartUploads'),
+        listObjectsA: s3ErrorBack('listObjects'),
+        listObjectsV2A: s3ErrorBack('listObjectsV2'),
+        listObjectVersionsA: s3ErrorBack('listObjectVersions'),
+        listPartsA: s3ErrorBack('listParts'),
+        putBucketAccelerateConfigurationA: s3ErrorBack('putBucketAccelerateConfiguration'),
+        putBucketAclA: s3ErrorBack('putBucketAcl'),
+        putBucketAnalyticsConfigurationA: s3ErrorBack('putBucketAnalyticsConfiguration'),
+        putBucketCorsA: s3ErrorBack('putBucketCors'),
+        putBucketEncryptionA: s3ErrorBack('putBucketEncryption'),
+        putBucketInventoryConfigurationA: s3ErrorBack('putBucketInventoryConfiguration'),
+        putBucketLifecycleConfigurationA: s3ErrorBack('putBucketLifecycleConfiguration'),
+        putBucketLoggingA: s3ErrorBack('putBucketLogging'),
+        putBucketMetricsConfigurationA: s3ErrorBack('putBucketMetricsConfiguration'),
+        putBucketNotificationConfigurationA: s3ErrorBack('putBucketNotificationConfiguration'),
+        putBucketPolicyA: s3ErrorBack('putBucketPolicy'),
+        putBucketReplicationA: s3ErrorBack('putBucketReplication'),
+        putBucketRequestPaymentA: s3ErrorBack('putBucketRequestPayment'),
+        putBucketTaggingA: s3ErrorBack('putBucketTagging'),
+        putBucketVersioningA: s3ErrorBack('putBucketVersioning'),
+        putBucketWebsiteA: s3ErrorBack('putBucketWebsite'),
+        putObjectA: s3ErrorBack('putObject'),
+        putObjectAclA: s3ErrorBack('putObjectAcl'),
+        putObjectTaggingA: s3ErrorBack('putObjectTagging'),
+        restoreObjectA: s3ErrorBack('restoreObject'),
+        uploadA: uploadA,
+        uploadPartA: s3ErrorBack('uploadPart'),
+        uploadPartCopyA: s3ErrorBack('uploadPartCopy'),
+        waitForA: waitForA
   		};
   	})(awsS3(s3Config));
   };
